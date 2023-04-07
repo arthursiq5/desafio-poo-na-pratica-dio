65c3ef3cc6dec881f3f545ae5d222ec36abafb59
arthursiq5 <arthur.siq5@gmail.com>
2023-04-06 23:05:07 -0300

    Adicionadas especificações ausentes

--- src/io/github/arthursiq5/dio/desafio_poo/src/Mentoria.java
+++ src/io/github/arthursiq5/dio/desafio_poo/src/Mentoria.java
@@ -4,13 +4,34 @@
  */
 package io.github.arthursiq5.dio.desafio_poo.src;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author arthur
  */
 public class Mentoria extends Conteudo {
+    private LocalDate data;
+
     @Override
     public double calcularXp() {
         return Mentoria.XP_PADRAO + 20d;
+    }
+    
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
     }
 }
