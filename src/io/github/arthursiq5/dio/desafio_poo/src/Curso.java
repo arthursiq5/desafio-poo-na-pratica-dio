076282c90361c13235b70971d7bddc83a9ee2fea
arthursiq5 <arthur.siq5@gmail.com>
2023-04-06 22:50:17 -0300

    Sobrescrita função toString de cursos para mostrar detalhes

--- src/io/github/arthursiq5/dio/desafio_poo/src/Curso.java
+++ src/io/github/arthursiq5/dio/desafio_poo/src/Curso.java
@@ -23,4 +23,13 @@ public class Curso extends Conteudo {
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
     }
+    
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
 }
